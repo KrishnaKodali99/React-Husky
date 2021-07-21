1e3c48489cf18c02a179f237942e09df323cbe14
Krishna Kodali <kodali.krishna@publicissapient.com>
2021-07-21 12:28:34 +0530

    test: added test cases to Alert

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,29 +1,34 @@
 import React from 'react'
+import './Alert.scss'
 
-// const Alert:React.FC<{type:string, message:string}> = ({type, message}) => {
-// // const Alert:React.FC({type,message}: PropsWithChildren<{type:string,message:string}>) => {
-//     if(type === "danger"){
-//         return (
-//             <div className="alert alert-danger" role="alert">
-//                 {message}
-//             </div>
-//         )
-//     }
-//     else if(type === "info"){
-//         return (
-//             <div className="alert alert-info" role="alert">
-//                 {message}
-//             </div>
-//         )
-//     }
-//     if(type === "success"){
-//         return (
-//             <div className="alert alert-success" role="alert">
-//                 {message}
-//             </div>
-//         )
-//     }
-// }
+const Alert:React.FC<{type:string, message:string}> = ({type, message}) => {
 
-// export default Alert
+    if(type === "danger"){
+        return (
+            <div className="alert alert-danger" role="alert">
+                {message}
+            </div>
+        )
+    }
+    else if(type === "info"){
+        return (
+            <div className="alert alert-info" role="alert">
+                {message}
+            </div>
+        )
+    }
+    else if(type === "success"){
+        return (
+            <div className="alert alert-success" role="alert">
+                {message}
+            </div>
+        )
+    }
+    else return (
+        <div>
+            <h1>Alert not working</h1>
+        </div>
+    )
+}
 
+export default Alert
---
+++ src/components/Alert/__tests__/Alert.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Alert from "../Alert";
+
+describe(("Testing <Alert />"), () => {
+    test(("Testing danger alert message"), () => {
+        render(<Alert type="danger" message="danger alert message"/>)
+        screen.getByText(/danger alert message/i)
+    })
+
+    test(("Testing info alert message"), () => {
+        render(<Alert type="info" message="info alert message"/>)
+        screen.getByText(/info alert message/i)
+    })
+
+    test(("Testing success alert message"), () => {
+        render(<Alert type="success" message="success alert message"/>)
+        screen.getByText(/success alert message/i)
+    })
+
+    test(("Testing any other alert message"), () => {
+        render(<Alert type="test-alert" message="test alert message"/>)
+        screen.getByText(/alert not working/i)
+    })
+})
